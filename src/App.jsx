87cc62935ee520d630255c4abae9f0a7cb135783
Vitali <3134114@mail.ru>
2025-09-04 11:03:51 +0300

    Заполнение БД

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
 import styled from 'styled-components'
+
 
 const Container = styled.div`
   display: flex;
@@ -15,11 +16,20 @@ const Container = styled.div`
 
 function App() {
 
+  const [name, setName] = useState('')
+
+  useEffect(()=>{
+    fetch('http://localhost:3005/users/1')
+    .then(response => response.json())
+    .then(json => setName(json.login))
+  },[])
+
 
   return (
     <>
     <Container>
        <i className="fa fa-address-book" aria-hidden="true"></i>  
+       {name}
     </Container>
     
     </>
